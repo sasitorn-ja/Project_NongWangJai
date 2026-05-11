2ab12c37b694ca7d4fc67c06728691898140574c
Sasitorn <sasitorn@MacBook-Pro-khxng-Sasitorn.local>
2026-05-11 08:20:51 +0700

    up apicpac

---
+++ api/dealers/[dealerId]/sites.ts
@@ -0,0 +1,10 @@
+import { proxyToCpac } from "../../../lib/cpac-proxy.js";
+
+function resolveDealerId(request: Request) {
+  const url = new URL(request.url);
+  return url.pathname.split("/").filter(Boolean)[2] ?? "";
+}
+
+export async function GET(request: Request) {
+  return proxyToCpac(request, `/api/ai-wangjai/dealers/${resolveDealerId(request)}/sites`);
+}
